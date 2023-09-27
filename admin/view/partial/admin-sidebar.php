e7a9ec2c349d894331355630141e1bd964890477
Md. Hossni Mubarak <mhmrajib@gmail.com>
2023-09-28 04:59:06 +0600

    Donate part removed from admin sidebar

--- admin/view/partial/admin-sidebar.php
+++ admin/view/partial/admin-sidebar.php
@@ -18,13 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
         </div>
     </div>
     <div class="postbox">
-        <h3 class="hndle"><span>Buy us a coffee</span></h3>
-        <div class="inside centered">
-            <p>If you like the plugin, would you like to support the advancement of this plugin?</p>
-            <p style="margin-bottom: 1px! important;"><a href='https://www.paypal.me/mhmrajib' class="button button-primary jobwp-button" target="_blank">Donate</a></p>
-        </div>
-    </div>
-    <div class="postbox">
         <h3 class="hndle"><span>Join us on facebook</span></h3>
         <div class="inside centered">
         <p style="margin-bottom: 1px! important;"><a href='https://www.facebook.com/hmplugin' class="button button-info" target="_blank">Join HM Plugin<span class="dashicons dashicons-facebook" style="position: relative; top: 3px; margin-left: 3px; color: #0fb9da;"></span></a></p>
